55845ae5ac64a76e8afb89cc6666f1e484b2c812
RanjivGyawali <86100867+RanjivGyawali@users.noreply.github.com>
2022-09-08 07:03:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+function setup() {
+    canvas = createCanvas(640, 480);
+    canvas.position(70, 200);
+    video = createCapture(VIDEO);
+    video.hide();
+    tint_color = "";
+}
+
+function draw() {
+    image(video, 100, 80, 300, 250);
+    stroke(2000, 10, 10);
+    fill(2000, 0, 0);
+    circle(40, 40, 50);
+    circle(460, 40, 50);
+    circle(40, 360, 50);
+    circle(460, 360, 50);
+    stroke(50, 168, 82);
+    fill(50, 168, 82);
+    rect(63, 27, 375, 25);
+    rect(63, 347, 375, 25);
+    rect(27, 63, 25, 275);
+    rect(448, 63, 25, 275);
+}
+
+function take_snapshot() {
+    save("frame.png");
+}
